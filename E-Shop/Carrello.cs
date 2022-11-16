d0881df2934805cdf85203f027c6dad2b323c93f
Aronne Ghilardi <aronneghilardi05@gmail.com>
2022-11-16 19:14:27 +0100

    Aggiungere i file di progetto.

---
+++ E-Shop/Carrello.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Shop
+{
+    class Carrello
+    {
+        private int _id;
+        string[] prodotto = new string[1000];
+        private int counter;
+
+        public Carrello(int id)
+        {
+            Id = id;
+        }
+
+        public int Id
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                _id = value;
+            }
+        }
+
+        public void Aggiungi(Prodotto x)
+        {
+            x.Id = prodotto[counter];
+            counter++;
+        }
+
+        public void Rimuovi(Prodotto x)
+        {
+            x.Id = prodotto[counter];
+            counter--;
+        }
+
+        public int getProdotti()
+        {
+            return counter;
+        }
+
+        public void Svuota()
+        {
+            counter = 0;
+        }
+    }
+}
---
+++ E-Shop/Prodotto.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Shop
+{
+    class Prodotto
+    {
+        private string _nome;
+        private string _marca;
+        private int _id;
+        private float _prezzo;
+
+        public Prodotto(string nome, string marca, int id, float prezzo)
+        {
+            Nome = nome;
+            Marca = marca;
+            Id = id;
+            Prezzo = prezzo;
+        }
+
+        public string Nome
+        {
+            get
+            {
+                return _nome;
+            }
+            set
+            {
+                _nome = value;
+            }
+        }
+
+        public string Marca
+        {
+            get
+            {
+                return _marca;
+            }
+            set
+            {
+                _marca = value;
+            }
+        }
+
+        public int Id
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                _id = value;
+            }
+        }
+
+        public float Prezzo
+        {
+            get
+            {
+                return _prezzo;
+            }
+            set
+            {
+                _prezzo = value;
+            }
+        }
+    }
+}
